4066a32bd21e2dbadb3be9df5e5e1470d07b775d
Shruti Agrawal <72216138+shruti0706@users.noreply.github.com>
2022-05-05 10:35:05 +0530

    Update firebase-config.js

--- web/src/firebase-config.js
+++ web/src/firebase-config.js
@@ -6,9 +6,19 @@
  * 3. Select Config from the Firebase SDK snippet pane.
  * 4. Copy the config object snippet, then add it here.
  */
-const config = {
+
   /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBNGit1ezK7WSM1-vYm2Fzttf_hxx3iBwM",
+  authDomain: "react-firechat-22550.firebaseapp.com",
+  projectId: "react-firechat-22550",
+  storageBucket: "react-firechat-22550.appspot.com",
+  messagingSenderId: "796320273844",
+  appId: "1:796320273844:web:accb537a499cc4a9ef0ee1",
+  measurementId: "G-26HPVW821M"
 };
+
 
 export function getFirebaseConfig() {
   if (!config || !config.apiKey) {
